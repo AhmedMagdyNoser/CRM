106351a5963acd756733b35ac9dd201cfca64aa6
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-02-28 12:28:28 +0200

    Add the basic structure ForgetPassword page

--- src/pages/auth/ForgetPassword.jsx
+++ src/pages/auth/ForgetPassword.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { globalErrorMessages } from '../../utils/utils';
 
 function ForgetPassword() {
-  return <div>ForgetPassword</div>;
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email) {
+      try {
+        if (error) setError('');
+        setLoading(true);
+        // Send the data to the server
+        // Navigate to the email verification page
+      } catch (error) {
+        setLoading(false);
+        setError(error.response?.data || globalErrorMessages);
+      }
+    } else {
+      // If the submit button is enabled with JS hacks
+      setError('Please enter a valid email');
+    }
+  }
+  return (
+    <section>
+      <h1>Forgot password</h1>
+      <p>Please enter the details below</p>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required autoFocus />
+        </div>
+        <div>
+          <button type="submit" disabled={!email || loading}>
+            {loading ? 'Loading...' : 'Continue'}
+          </button>
+        </div>
+        {error && <div>{error}</div>}
+      </form>
+      <Link to="/login">← Back</Link>
+    </section>
+  );
 }
 
 export default ForgetPassword;
