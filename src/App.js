8afaf386df2e6f9ff1dd288473be82df066f7985
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-02-28 08:22:03 +0200

    Add initial routes and components for authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/routes/Layout";
+import NotFound from "./pages/routes/NotFound";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Home from "./pages/users/Home";
+
 function App() {
-  return <div>Hello, World!</div>;
+  console.log("Rendering App");
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
