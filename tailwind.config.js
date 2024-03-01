4ca4a5f00b2e48b7f8e5092f6b78974d8eabf121
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-01 11:17:10 +0200

    Add new colors and animation to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  extend: {
+    colors: {
+      pro: {
+        100: "#f3f7ff", // Light Background
+        200: "#e7f0ff", // Light Background Hover
+        300: "#3b84fd", // Dark Background Hover
+        400: "#0a65fc", // Dark Background + Default Text
+        500: "#061257", // Dark Text
+      },
+    },
+    animation: {
+      "spin-slow": "spin 2.5s linear infinite",
+    },
+  },
 };
