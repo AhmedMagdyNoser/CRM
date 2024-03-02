f21a139a9c09565b22d30a0bc3d3f6157fcb1393
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-02 21:21:04 +0200

    Refactor Checkbox component to add keyboard accessibility

--- src/components/global/Checkbox.jsx
+++ src/components/global/Checkbox.jsx
@@ -3,8 +3,20 @@ import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Checkbox({ label, checked, onClick }) {
+  const handleKeyDown = (event) => {
+    if (event.key === ' ' || event.key === 'Spacebar' || event.key === 'Enter') {
+      event.preventDefault();
+      onClick();
+    }
+  };
+
   return (
-    <div className="text-progray-300 flex cursor-pointer items-center gap-1" onClick={onClick}>
+    <div
+      tabIndex={0}
+      className="text-progray-300 flex cursor-pointer items-center gap-1"
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+    >
       <FontAwesomeIcon icon={checked ? faCheckSquare : faSquare} className="text-lg text-pro-300" />
       <p className="select-none">{label}</p>
     </div>
