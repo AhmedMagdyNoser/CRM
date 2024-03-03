3b6fc44f587cf68087129669478ba26f9e076eca
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-03 10:15:29 +0200

    Add styles to the ForgotPassword page

--- src/pages/auth/ForgetPassword.jsx
+++ src/pages/auth/ForgetPassword.jsx
@@ -3,6 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import { globalErrorMessage } from '../../utils/utils';
 import { validationRegex } from '../../utils/utils';
 import axios from '../../api/axios';
+import forgotPassword from '../../assets/forgotPassword.svg';
+import InputField from '../../components/global/InputField';
+import { faArrowLeft, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import SubmitButton from '../../components/global/SubmitButton';
+import ErrorAlert from '../../components/global/ErrorAlert';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ForgetPassword() {
   const navigate = useNavigate();
@@ -18,7 +24,7 @@ function ForgetPassword() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    if (email) {
+    if (validEmail) {
       try {
         if (error) setError('');
         setLoading(true);
@@ -38,24 +44,44 @@ function ForgetPassword() {
       setError('Please enter a valid email');
     }
   }
+
   return (
-    <section>
-      <h1>Forgot password</h1>
-      <p>Please enter the details below</p>
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required autoFocus />
+    <div className="flex h-screen items-center justify-center bg-progray-50">
+      <form
+        onSubmit={handleSubmit}
+        autoComplete="off"
+        className="flex h-full w-full animate-fade-in-fast flex-col justify-between gap-3 bg-white p-6 sm:h-fit sm:w-[500px] sm:rounded-xl sm:p-12 sm:shadow-lg lg:w-[650px]"
+      >
+        <div className="flex flex-col items-center gap-3">
+          <div className="w-[215px]">
+            <img className="w-full" src={forgotPassword} alt="Forgot password" />
+          </div>
+          <h1 className="text-center text-2xl font-bold capitalize text-progray-300 sm:text-3xl">Forgot password</h1>
+          <p className="text-center text-progray-200">Please Enter your email address below</p>
+          <InputField
+            type="email"
+            placeholder="Email"
+            icon={faEnvelope}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            maxLength={50}
+            autoFocus
+            required
+          />
         </div>
-        <div>
-          <button type="submit" disabled={!validEmail || loading}>
-            {loading ? 'Loading...' : 'Continue'}
+        <div className="flex flex-col gap-3">
+          {error && <ErrorAlert message={error} />}
+          <SubmitButton label="Continue" loading={loading} disabled={!validEmail || loading} />
+          <button
+            onClick={() => navigate(-1)}
+            className="self-start rounded-md px-5 py-2 uppercase text-progray-200 transition-colors hover:bg-progray-100"
+          >
+            <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
+            Back
           </button>
         </div>
-        {error && <div>{error}</div>}
       </form>
-      <button onClick={() => navigate(-1)}>← Back</button>
-    </section>
+    </div>
   );
 }
 
