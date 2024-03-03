0c10a886075d252f4fee9c1ee4d376790293e3a5
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-03 10:55:49 +0200

    Add MiniFormBox component and clean it

---
+++ src/components/auth/MiniFormBox.jsx
@@ -0,0 +1,55 @@
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import ErrorAlert from '../global/ErrorAlert';
+import SubmitButton from '../global/SubmitButton';
+
+function MiniFormBox({
+  onSubmit,
+  image,
+  title,
+  paragraph,
+  submitButtonLabel,
+  submitButtonDisabled,
+  loading,
+  error,
+  backButton,
+  children,
+}) {
+  const navigate = useNavigate();
+  return (
+    <div className="flex h-screen items-center justify-center bg-progray-50">
+      <form
+        onSubmit={onSubmit}
+        autoComplete="off"
+        className="flex h-full w-full animate-fade-in-fast flex-col justify-between gap-3 bg-white p-6 sm:h-fit sm:w-[500px] sm:rounded-xl sm:p-12 sm:shadow-lg lg:w-[650px]"
+      >
+        <div className="flex flex-col items-center gap-3">
+          {image && (
+            <div className="h-[215px] w-[215px]">
+              <img className="w-full" src={image} alt={title} />
+            </div>
+          )}
+          <h1 className="text-center text-2xl font-bold capitalize text-progray-300 sm:text-3xl">{title}</h1>
+          <p className="text-center text-progray-200">{paragraph}</p>
+          {children}
+        </div>
+        <div className="flex flex-col gap-3">
+          {error && <ErrorAlert message={error} />}
+          <SubmitButton label={submitButtonLabel} loading={loading} disabled={submitButtonDisabled || loading} />
+          {backButton && (
+            <button
+              onClick={() => navigate(-1)}
+              className="self-start rounded-md px-5 py-2 uppercase text-progray-200 transition-colors hover:bg-progray-100"
+            >
+              <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
+              Back
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default MiniFormBox;
--- src/pages/auth/ForgetPassword.jsx
+++ src/pages/auth/ForgetPassword.jsx
@@ -5,10 +5,8 @@ import { validationRegex } from '../../utils/utils';
 import axios from '../../api/axios';
 import forgotPassword from '../../assets/forgotPassword.svg';
 import InputField from '../../components/global/InputField';
-import { faArrowLeft, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import SubmitButton from '../../components/global/SubmitButton';
-import ErrorAlert from '../../components/global/ErrorAlert';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import MiniFormBox from '../../components/auth/MiniFormBox';
 
 function ForgetPassword() {
   const navigate = useNavigate();
@@ -46,42 +44,28 @@ function ForgetPassword() {
   }
 
   return (
-    <div className="flex h-screen items-center justify-center bg-progray-50">
-      <form
-        onSubmit={handleSubmit}
-        autoComplete="off"
-        className="flex h-full w-full animate-fade-in-fast flex-col justify-between gap-3 bg-white p-6 sm:h-fit sm:w-[500px] sm:rounded-xl sm:p-12 sm:shadow-lg lg:w-[650px]"
-      >
-        <div className="flex flex-col items-center gap-3">
-          <div className="w-[215px]">
-            <img className="w-full" src={forgotPassword} alt="Forgot password" />
-          </div>
-          <h1 className="text-center text-2xl font-bold capitalize text-progray-300 sm:text-3xl">Forgot password</h1>
-          <p className="text-center text-progray-200">Please Enter your email address below</p>
-          <InputField
-            type="email"
-            placeholder="Email"
-            icon={faEnvelope}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            maxLength={50}
-            autoFocus
-            required
-          />
-        </div>
-        <div className="flex flex-col gap-3">
-          {error && <ErrorAlert message={error} />}
-          <SubmitButton label="Continue" loading={loading} disabled={!validEmail || loading} />
-          <button
-            onClick={() => navigate(-1)}
-            className="self-start rounded-md px-5 py-2 uppercase text-progray-200 transition-colors hover:bg-progray-100"
-          >
-            <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
-            Back
-          </button>
-        </div>
-      </form>
-    </div>
+    <MiniFormBox
+      onSubmit={handleSubmit}
+      image={forgotPassword}
+      title="Forgot password"
+      paragraph="Please enter your email address below"
+      submitButtonLabel="Continue"
+      submitButtonDisabled={!validEmail}
+      loading={loading}
+      error={error}
+      backButton
+    >
+      <InputField
+        type="email"
+        placeholder="Email"
+        icon={faEnvelope}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        maxLength={50}
+        autoFocus
+        required
+      />
+    </MiniFormBox>
   );
 }
 
