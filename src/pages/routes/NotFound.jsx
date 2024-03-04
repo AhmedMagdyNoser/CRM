51f890ef6d2561cb7a28fb8d72948cc552dd0f94
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-04 11:34:07 +0200

    Add link to home page in NotFound component

--- src/pages/routes/NotFound.jsx
+++ src/pages/routes/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import pageNotFound from '../../assets/pageNotFound.svg';
 
 function NotFound() {
@@ -9,6 +10,9 @@ function NotFound() {
         </div>
         <h1 className="text-center">Page Not Found</h1>
         <p className="text-center text-sm sm:text-base">The page you are looking for does not exist.</p>
+        <Link to="/" className="btn-secondary px-5 py-2">
+          Back to Home
+        </Link>
       </div>
     </div>
   );
