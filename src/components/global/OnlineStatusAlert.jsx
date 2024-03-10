fe829a57394385024bf80ce8739cc6143ee071af
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-10 10:22:51 +0200

    Fix a bug in OnlineStatusAlert component and update its style

--- src/components/global/OnlineStatusAlert.jsx
+++ src/components/global/OnlineStatusAlert.jsx
@@ -10,12 +10,12 @@ function OnlineStatueAlert() {
   useEffect(() => {
     if (!online) {
       setShow(true);
-    } else if (show) {
+    } else {
       setTimeout(() => {
         setShow(false);
       }, 2500); // Online status alert duration
     }
-  }, [online, show]);
+  }, [online]);
 
   if (!show) {
     return null;
@@ -25,7 +25,7 @@ function OnlineStatueAlert() {
     <div className="fixed bottom-0 right-0 z-40 w-full animate-fade-in-fast p-5 sm:w-[450px]">
       <div
         className="flex flex-wrap items-center justify-between gap-3 bg-white p-5"
-        style={{ boxShadow: '0px 0px 5px 0px #0002' }}
+        style={{ boxShadow: '0px 0px 5px 0px #0001, 0 1px 3px 0 #0002, 0 1px 2px -1px #0002' }}
       >
         <div className="flex-center flex-wrap gap-3">
           <FontAwesomeIcon icon={faWifi} className={'sm:text-xl ' + (online ? 'text-green-600' : 'text-progray-200')} />
