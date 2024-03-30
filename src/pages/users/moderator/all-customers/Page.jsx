2c47da24a2f3a7c43c52dd84dbc3018a9f8fe785
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-03-30 02:07:55 +0200

    Refactor AllCustomers component and use useOnLoadFetch hook for fetching customers data

--- src/pages/users/moderator/all-customers/Page.jsx
+++ src/pages/users/moderator/all-customers/Page.jsx
@@ -6,46 +6,21 @@ import { roles } from '../../../../utils/utils';
 import AllCustomersHeaderSection from './components/header-section/Section';
 import LastWeekCustomersSection from './components/last-week-customers-section/Section';
 import AllCustomersSection from './components/all-customers-section/Section';
+import useOnLoadFetch from '../../../../hooks/useOnLoadFetch';
 
 function AllCustomers() {
   useDocumentTitle('All Customers');
+
   const { auth } = useAuth();
-  const privateAxios = usePrivateAxios();
 
-  const [customers, setCustomers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    let controller = new AbortController();
-    let canceled = false;
-
-    if (auth.roles.includes(roles.moderator)) {
-      (async function getAllCustomers() {
-        try {
-          const { data } = await privateAxios({ url: '/moderator/get-all-customers', signal: controller.signal });
-          if (!canceled) setCustomers(data);
-        } catch (error) {
-          if (!canceled) console.dir(error);
-        } finally {
-          if (!canceled) setLoading(false);
-        }
-      })();
-    }
-
-    return () => {
-      canceled = true;
-      controller.abort();
-    };
-  }, [privateAxios, auth.roles]);
+  const { data: customers, loading } = useOnLoadFetch(`/moderator/get-all-customers`);
 
   return (
-    auth.roles.includes(roles.moderator) && (
-      <section className="flex flex-col gap-3">
-        <AllCustomersHeaderSection />
-        <LastWeekCustomersSection customers={customers} loading={loading} />
-        <AllCustomersSection customers={customers} loading={loading} />
-      </section>
-    )
+    <section className="flex flex-col gap-3">
+      <AllCustomersHeaderSection />
+      <LastWeekCustomersSection customers={customers} loading={loading} />
+      <AllCustomersSection customers={customers} loading={loading} />
+    </section>
   );
 }
 
