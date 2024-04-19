fe59bdf413a21a850c1e1868e6285e2696a4b028
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-04-19 09:44:25 +0200

    Refactor CompanyInfo component and add loading and error handling
    - Refactored CompanyInfo component to use the useCompany hook
    - Added loading and error handling for company data retrieval
    - Updated component layout and added conditional rendering for loading and error states

--- src/pages/users/CompanyInfo.jsx
+++ src/pages/users/CompanyInfo.jsx
@@ -1,11 +1,37 @@
+import useCompany from '../../hooks/useCompany';
 import useDocumentTitle from '../../hooks/useDocumentTitle';
+import Alert from '../../components/ui/Alert';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import icons from '../../utils/faIcons';
 
 function CompanyInfo() {
   useDocumentTitle('Company Info');
+  
+  const { company } = useCompany();
 
   return (
-    <section>
-      <h1>Company</h1>
+    <section className="flex flex-col gap-8">
+      <h1>Company Info</h1>
+      {company.loading ? (
+        <div className="flex-center h-72 flex-col gap-4 rounded-xl bg-gray-100">
+          <FontAwesomeIcon icon={icons.spinner} spin className="text-4xl text-pro-300" />
+          <p className="">Loading company info...</p>
+        </div>
+      ) : company.error ? (
+        <Alert.Error message={company.error} />
+      ) : (
+        <div className="col-span-2 flex flex-col gap-8">
+          <div>
+            <p>Company name</p>
+            <p className="mt-2 rounded-xl bg-gray-100 p-5 text-3xl font-bold text-gray-800">{company.data.name}</p>
+          </div>
+
+          <div>
+            <p>Description</p>
+            <p className="mt-2 rounded-xl bg-gray-100 p-5 text-lg">{company.data.description}</p>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
