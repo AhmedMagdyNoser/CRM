aabc9cc85f88fa76d247453a08313c794b642ce1
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-04-23 19:19:51 +0200

    Refactor InterestsSection component to include a button for adding new interests

--- src/pages/inner/company-info/components/interests-section/Section.jsx
+++ src/pages/inner/company-info/components/interests-section/Section.jsx
@@ -17,7 +17,15 @@ export default function InterestsSection() {
 
   return (
     <section className="flex flex-1 flex-col gap-4 rounded-xl bg-gray-100 p-6">
-      <h2 className="text-2xl font-bold">Interests</h2>
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold">Interests</h2>
+        {!(interests.loading || interests.error) && (
+          <button className="btn-primary flex-center gap-2 rounded-xl p-3 px-5 text-xs sm:text-sm">
+            <FontAwesomeIcon icon={icons.plus} />
+            <span>New Interest</span>
+          </button>
+        )}
+      </div>
       {interests.loading ? (
         <div className="flex-center h-72 flex-col gap-4">
           <FontAwesomeIcon icon={icons.spinner} spin className="text-4xl text-pro-300" />
