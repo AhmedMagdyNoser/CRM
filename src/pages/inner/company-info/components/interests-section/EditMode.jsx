dd41f139210add135896e7b64124c5bedc635120
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-04-24 14:27:03 +0200

    Add update interest functionality

--- src/pages/inner/company-info/components/interests-section/EditMode.jsx
+++ src/pages/inner/company-info/components/interests-section/EditMode.jsx
@@ -1,7 +1,46 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { globalErrorMessage } from '../../../../../utils/utils';
+import usePrivateAxios from '../../../../../hooks/usePrivateAxios';
+import useInterests from '../../../../../hooks/useInterests';
+import icons from '../../../../../utils/faIcons';
 
 function EditMode({ interest, setEditMode }) {
+  const privateAxios = usePrivateAxios();
+
+  const { setInterests } = useInterests();
+
   const [name, setName] = useState(interest.name);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function handleUpdate(e) {
+    e.preventDefault();
+    try {
+      setError('');
+      setLoading(true);
+      const res = await privateAxios({
+        method: 'put',
+        url: '/manager/update-interest',
+        data: { id: interest.id, name, isDisabled: interest.isDisabled },
+      });
+      setInterests((prev) => {
+        const prevEnabled = prev.data.enabled;
+        prevEnabled[prevEnabled.findIndex((prevInterest) => prevInterest.id === interest.id)] = res.data;
+        return {
+          data: { disabled: prev.data.disabled, enabled: prevEnabled },
+          loading: false,
+          error: '',
+        };
+      });
+      setEditMode(false);
+    } catch (error) {
+      setError((error.response?.data?.errors && error.response.data.errors[0]) || globalErrorMessage);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <>
@@ -11,11 +50,28 @@ function EditMode({ interest, setEditMode }) {
         placeholder="Interest Name"
         className="flex-1 px-3 outline-none placeholder:font-normal"
         autoFocus
+        size={1}
       />
+
       <div className="flex gap-2">
-        <button className="btn-secondary flex-center h-10 rounded-xl px-4 text-sm font-semibold">
-          {/* <FontAwesomeIcon icon={icons.spinner} spin /> */}
-          Update
+        {error && (
+          <div className="flex-center h-10 rounded-xl bg-red-100 px-4 text-sm text-red-500">
+            <FontAwesomeIcon icon={icons.exclamationCircle} />
+          </div>
+        )}
+        <button
+          onClick={handleUpdate}
+          className="btn-secondary flex-center h-10 gap-2 rounded-xl px-4 text-sm font-semibold"
+          disabled={loading || !name}
+        >
+          {loading ? (
+            <>
+              <FontAwesomeIcon icon={icons.spinner} spin />
+              <span>Updating</span>
+            </>
+          ) : (
+            <span>Update</span>
+          )}
         </button>
         <button
           onClick={() => setEditMode(false)}
