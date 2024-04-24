f9d31da27f47d113d2f90f1d0056d4e212eb0551
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-04-24 11:17:59 +0200

    Refactor InterestCard component to include edit functionality

--- src/pages/inner/company-info/components/interests-section/InterestCard.jsx
+++ src/pages/inner/company-info/components/interests-section/InterestCard.jsx
@@ -1,9 +1,36 @@
+import { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useHover from '../../../../../hooks/useHover';
+import icons from '../../../../../utils/faIcons';
+
 export default function InterestCard({ interest }) {
+  const [showEdit, setShowEdit] = useState(false);
+  const element = useRef(null);
+
+  useHover(
+    element,
+    () => setShowEdit(true),
+    () => setShowEdit(false),
+  );
+
   return (
     <div
-      className={`rounded-xl bg-white p-5 text-lg font-bold -tracking-wider shadow-sm ${interest.isDisabled ? 'text-gray-500' : 'text-gray-800'}`}
+      ref={element}
+      className={`flex h-[75px] animate-fade-in-fast items-center justify-between rounded-xl bg-white text-lg font-bold -tracking-wider shadow-sm ${interest.isDisabled ? 'text-gray-500' : 'text-gray-800'}`}
     >
-      {interest.name}
+      <div className="flex w-full items-center justify-between p-3">
+        <span className="px-3">{interest.name}</span>
+        {showEdit && (
+          <div className="flex animate-fade-in-fast gap-2 px-1">
+            <button className="btn-danger flex-center h-10 w-10 rounded-xl">
+              <FontAwesomeIcon icon={icons.trash} />
+            </button>
+            <button className="btn-secondary flex-center h-10 w-10 rounded-xl">
+              <FontAwesomeIcon icon={icons.edit} />
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
