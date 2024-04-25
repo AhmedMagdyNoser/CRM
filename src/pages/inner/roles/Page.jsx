09081014d6890da0ad9dc0a77d6d871712192e25
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-04-25 13:38:00 +0200

    Add stats section to Roles page
    This commit adds a new stats section to the Roles page. The stats section displays various statistics about the users, such as the number of managers, marketing moderators, sales representatives, users with no roles, and unconfirmed accounts. The stats are calculated based on the data fetched from the server. The commit also includes the necessary components and styles for the stats section.

--- src/pages/inner/roles/Page.jsx
+++ src/pages/inner/roles/Page.jsx
@@ -1,16 +1,18 @@
 import useDocumentTitle from '../../../hooks/useDocumentTitle';
 import useOnLoadFetch from '../../../hooks/useOnLoadFetch';
+import StatsSection from './components/stats-section/Section';
 import UserSection from './components/users-secion/Section';
 
 export default function Roles() {
   useDocumentTitle('Roles');
 
-  const { data: users } = useOnLoadFetch('/manager/get-all-users');
+  const { data: users, loading, error } = useOnLoadFetch('/manager/get-all-users');
 
   return (
     <div className="flex flex-col gap-4">
       <h1>Roles</h1>
-      <UserSection users={users} />
+      <StatsSection users={users} loading={loading} error={error} />
+      <UserSection users={users} loading={loading} error={error} />
     </div>
   );
 }
---
+++ src/pages/inner/roles/components/stats-section/Section.jsx
@@ -0,0 +1,55 @@
+import StatsCard from './StatsCard';
+
+export default function StatsSection({ users, loading, error }) {
+  let stats;
+
+  if (users) stats = getUsersStats(users);
+
+  return (
+    !error && (
+      <section className="flex flex-col gap-2 rounded-xl bg-gray-50 p-4">
+        <p>Users Statistics</p>
+        <div className="scrollbar-hide flex gap-3 overflow-x-auto py-1">
+          <StatsCard title="Managers" value={stats.managers} loading={loading} />
+          <StatsCard title="Marketing Moderators" value={stats.marketingModerators} loading={loading} />
+          <StatsCard title="Sales Representatives" value={stats.salesRepresentatives} loading={loading} />
+          <StatsCard title="Users With No Roles" value={stats.noRoles} loading={loading} />
+          <StatsCard title="Unconfirmed Accounts" value={stats.unconfirmed} loading={loading} />
+        </div>
+      </section>
+    )
+  );
+}
+
+function getUsersStats(users) {
+  const stats = {
+    managers: 0,
+    marketingModerators: 0,
+    salesRepresentatives: 0,
+    noRoles: 0,
+    unconfirmed: 0,
+  };
+
+  users.forEach((user) => {
+    if (!user.emailConfirmed) {
+      stats.unconfirmed++;
+    } else {
+      const numRoles = user.roles ? user.roles.length : 0;
+      switch (numRoles) {
+        case 3:
+          stats.managers++;
+          break;
+        case 2:
+          stats.marketingModerators++;
+          break;
+        case 1:
+          stats.salesRepresentatives++;
+          break;
+        default:
+          stats.noRoles++;
+      }
+    }
+  });
+
+  return stats;
+}
