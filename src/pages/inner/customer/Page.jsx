f7feb6bc66b8c3b332ae19f023608088c0350d37
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-04-28 11:51:35 +0300

    Refactor customer page to allow the sales repreresentative view their customers
    - Refactored the customer page component to improve readability and maintainability.
    - Updated the API endpoints based on the user's role to fetch customer details and actions.
    - Added authentication hook to retrieve user roles.
    - Updated the API endpoints for fetching customer details and actions based on the user's role.

--- src/pages/inner/customer/Page.jsx
+++ src/pages/inner/customer/Page.jsx
@@ -6,11 +6,15 @@ import DetailsSection from './components/details-section/Section';
 import ActionsSection from './components/actions-section/Section';
 import CustomerHeaderSection from './components/header-section/Section';
 import Alert from '../../../components/ui/Alert';
+import useAuth from '../../../hooks/useAuth';
+import { roles } from '../../../utils/utils';
 
 function Customer() {
   useDocumentTitle('Customer Details');
 
   const params = useParams();
+
+  const { auth } = useAuth();
 
   const [editingMode, setEditingMode] = useState(false);
 
@@ -21,12 +25,16 @@ function Customer() {
     data: details,
     setData: setDetails,
     error: detailsError,
-  } = useOnLoadFetch(`/moderator/get-customer/${params.id}`);
+  } = useOnLoadFetch(
+    `/${auth.roles.includes(roles.moderator) ? 'moderator/get-customer' : 'SalesRep/GetCustomerAssignedToSales'}/${params.id}`,
+  );
   const {
     loading: loadingActions,
     data: actionsData,
     error: actionsError,
-  } = useOnLoadFetch(`/moderator/get-customer-actions/${params.id}`);
+  } = useOnLoadFetch(
+    `/${auth.roles.includes(roles.moderator) ? 'moderator/get-customer-actions' : 'SalesRep/ActionsForCustomersAssignedToSales'}/${params.id}`,
+  );
 
   return (
     <div className="flex h-full flex-col gap-4">
