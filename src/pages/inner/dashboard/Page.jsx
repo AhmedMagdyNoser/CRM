043ff04e337f752e890b862e8afbf95f358f1bfb
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-05-06 20:44:41 +0300

    Add DashboardHeaderSection component to Manager Dashboard page
    This commit adds the DashboardHeaderSection component to the Manager Dashboard page. The component includes a title and a set of buttons for selecting different periods of time.

--- src/pages/inner/dashboard/Page.jsx
+++ src/pages/inner/dashboard/Page.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import useDocumentTitle from '../../../hooks/useDocumentTitle';
-import DailyReport from './components/DailyReport';
+import DashboardHeaderSection from './components/header-section/Section';
 import GeneralSection from './components/GeneralSection';
+import DailyReport from './components/DailyReport';
+
+const periodOptions = [
+  { value: '', label: 'Total' },
+  { value: 'Daily', label: 'Today' },
+  { value: 'Weekly', label: 'Last Week' },
+  { value: 'Monthly', label: 'Last Month' },
+];
 
 function Dashboard() {
   useDocumentTitle('Manager Dashboard');
+  const [selectedPeriod, setSelectedPeriod] = useState(periodOptions[0]);
 
   return (
     <div className="flex flex-col gap-5">
-      <h1>Manager Dashboard</h1>
+      <DashboardHeaderSection
+        periodOptions={periodOptions}
+        selectedPeriod={selectedPeriod}
+        setSelectedPeriod={setSelectedPeriod}
+      />
+
       <p>Under construction...</p>
 
       <GeneralSection />
