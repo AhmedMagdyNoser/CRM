e2950127a715d11575896ea1fba46b182e8b6ee0
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-05-08 12:05:48 +0300

    Refactor Dashboard component to fetch data using privateAxios
    - Refactored the Dashboard component to use the `usePrivateAxios` hook for fetching data.
    - Added `useEffect` hook to fetch global statistics and sales stats data.
    - Added loading and error states to handle the loading and error scenarios.
    - Updated the rendering logic to display loading spinner or error message if applicable.
    - Passed the fetched data to the `GlobalStatistics` and `SalesStats` components.

--- src/pages/inner/dashboard/Page.jsx
+++ src/pages/inner/dashboard/Page.jsx
@@ -1,15 +1,51 @@
-import { useState } from 'react';
-import { periodOptions } from '../../../utils/utils';
+import { useEffect, useState } from 'react';
+import { globalErrorMessage, periodOptions } from '../../../utils/utils';
 import useDocumentTitle from '../../../hooks/useDocumentTitle';
 import DashboardHeaderSection from './components/header-section/Section';
 import GlobalStatistics from './components/GlobalStatistics';
 import SalesStats from './components/SalesStats';
 import Loading from './components/status/Loading';
+import usePrivateAxios from '../../../hooks/usePrivateAxios';
 
 function Dashboard() {
   useDocumentTitle('Manager Dashboard');
 
   const [selectedPeriod, setSelectedPeriod] = useState(periodOptions[0]);
+
+  const privateAxios = usePrivateAxios();
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    let controller = new AbortController();
+    let canceled = false;
+
+    (async function fetchData() {
+      try {
+        const results = {};
+        await Promise.all([
+          privateAxios({ url: '/reports/global-statistics', signal: controller.signal }).then(
+            (response) => (results.globalStatistics = response.data),
+          ),
+          privateAxios({ url: `/reports/main-report?within=${selectedPeriod.value}`, signal: controller.signal }).then(
+            (response) => (results.salesStats = response.data),
+          ),
+        ]);
+        if (!canceled) setData(results);
+      } catch (error) {
+        if (!canceled) setError((error.response?.data?.errors && error.response.data.errors[0]) || globalErrorMessage);
+      } finally {
+        if (!canceled) setLoading(false);
+      }
+    })();
+
+    return () => {
+      canceled = true;
+      controller.abort();
+    };
+  }, [privateAxios]);
 
   return (
     <div className="flex h-full flex-col gap-5">
@@ -17,8 +53,16 @@ function Dashboard() {
 
       <p>Under construction...</p>
 
-      <GlobalStatistics period={selectedPeriod} />
-      <SalesStats period={selectedPeriod} />
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <div className="flex-center">{error}</div>
+      ) : (
+        <>
+          <GlobalStatistics data={data.globalStatistics} period={selectedPeriod} />
+          <SalesStats data={data.salesStats} period={selectedPeriod} />
+        </>
+      )}
     </div>
   );
 }
