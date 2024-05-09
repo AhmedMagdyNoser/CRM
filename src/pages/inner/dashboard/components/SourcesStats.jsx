e60db77e94981df474b679e5e5063bd316d0b349
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-05-09 12:17:41 +0300

    Refactor SourcesStats component and add conditional rendering for no data case

--- src/pages/inner/dashboard/components/SourcesStats.jsx
+++ src/pages/inner/dashboard/components/SourcesStats.jsx
@@ -1,12 +1,13 @@
-import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import noData from '../../../../assets/noData.svg';
 
 Chart.register(ArcElement, Tooltip, Legend);
 
 const colors = ['#E94144', '#F3722C', '#F8961E', '#F9C74F', '#90BE6D', '#43AA8B', '#577590'];
 
 export default function SourcesStats({ data }) {
-  data = data.filter((item) => item.count);
+  data = data.filter((item) => item.count); // Remove items with 0 count
 
   const dataset = {
     labels: data.map((item) => item.name),
@@ -24,22 +25,35 @@ export default function SourcesStats({ data }) {
   return (
     <div className="rounded-3xl bg-gray-100 p-6">
       <h2 className="mb-4">Customer Source Breakdown</h2>
-      <Doughnut
-        data={dataset}
-        options={{
-          plugins: {
-            legend: {
-              align: 'start',
+      {data.length === 0 ? (
+        <div className="flex-center flex-col gap-4 py-6">
+          <img src={noData} alt="No Data" className="w-1/4" />
+          <p>No data available for this period.</p>
+        </div>
+      ) : (
+        <Doughnut
+          data={dataset}
+          options={{
+            plugins: {
+              legend: {
+                align: 'start',
+                labels: {
+                  usePointStyle: true,
+                  font: {
+                    size: 12.5,
+                  },
+                },
+              },
+              tooltip: {
+                backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                displayColors: false, // Hide color box in tooltip body
+                cornerRadius: 10,
+                padding: 10,
+              },
             },
-            tooltip: {
-              backgroundColor: 'rgba(0, 0, 0, 0.75)',
-              displayColors: false, // Hide color box in tooltip body
-              cornerRadius: 10,
-              padding: 10,
-            },
-          },
-        }}
-      />
+          }}
+        />
+      )}
     </div>
   );
 }
