7557619e444ad4716295b60f523f4fdbc3b150dc
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-05-10 12:01:02 +0300

    Display employee data in EmployeeReport component
    This commit adds code to display employee data in the EmployeeReport component. The employee data will be shown in the section element below the existing code. The console.log statement used for debugging purposes has also been added and should be removed before committing.

--- src/pages/inner/dashboard/employee-stats/components/EmployeeReport.jsx
+++ src/pages/inner/dashboard/employee-stats/components/EmployeeReport.jsx
@@ -1,4 +1,27 @@
-export default function EmployeeReport({ employee }) {
-  return <div>{JSON.stringify(employee)}</div>;
-}
+import InterestsStats from '../../../assigned-customers/InterestsStats';
+import SourcesStats from '../../components/SourcesStats';
 
+// IMPORTANT NOTES
+// I have displayed the doneDeals and sources data in section element blow. Don't remove it.
+// Your task is to display the employee other data like name, bestDeal, etc.
+
+export default function EmployeeReport({ employee }) {
+  console.log(employee); // See the data in console, then remove this line
+  
+  return (
+    <div>
+      {/* Display employee data here*/}
+
+      <section className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-5">
+        <div className="xl:col-span-3">
+          <InterestsStats data={employee.doneDeals} />
+        </div>
+        <div className="xl:col-span-2">
+          <SourcesStats data={employee.sources} />
+        </div>
+      </section>
+
+      {/* Or display it data here, or both*/}
+    </div>
+  );
+}
