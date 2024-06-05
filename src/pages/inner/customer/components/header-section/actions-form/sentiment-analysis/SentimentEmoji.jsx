41dae754fdc3fed39599048fba35267fe4261c4d
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-06-05 16:52:27 +0300

    Improve sentiment analysis in SentimentEmoji component

--- src/pages/inner/customer/components/header-section/actions-form/sentiment-analysis/SentimentEmoji.jsx
+++ src/pages/inner/customer/components/header-section/actions-form/sentiment-analysis/SentimentEmoji.jsx
@@ -1,20 +1,36 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import icons from "../../../../../../../utils/faIcons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import icons from '../../../../../../../utils/faIcons';
 
 function SentimentEmoji({ sentiment }) {
   return sentiment === 'error' || sentiment === 'idle' ? null : (
-    <span className="flex-center absolute bottom-2 right-2 h-8 w-8 rounded-xl bg-white text-sm sm:text-lg">
+    <span className="flex-center absolute bottom-2 right-2 gap-2 rounded-xl bg-white p-2 text-sm sm:text-lg">
       {sentiment === 'loading' && (
-        <FontAwesomeIcon title="Analyzing..." icon={icons.spinner} spin className="text-gray-500" />
+        <>
+          <FontAwesomeIcon title="Analyzing..." icon={icons.spinner} spin className="text-gray-500" />
+          <p>Analyzing...</p>
+        </>
       )}
       {sentiment === 'Positive' && (
-        <FontAwesomeIcon title="Positive" icon={icons.sentiment.positive} className="animate-fade-in-fast text-green-600" />
+        <>
+          <FontAwesomeIcon
+            title="Positive"
+            icon={icons.sentiment.positive}
+            className="animate-fade-in-fast text-green-600"
+          />
+          <p>Positive</p>
+        </>
       )}
       {sentiment === 'Negative' && (
-        <FontAwesomeIcon title="Negative" icon={icons.sentiment.negative} className="animate-fade-in-fast text-red-500" />
+        <>
+          <FontAwesomeIcon title="Negative" icon={icons.sentiment.negative} className="animate-fade-in-fast text-red-500" />
+          <p>Negative</p>
+        </>
       )}
       {(sentiment === 'Neutral' || sentiment === 'Irrelevant') && (
-        <FontAwesomeIcon title="Neutral" icon={icons.sentiment.neutral} className="animate-fade-in-fast text-orange-500" />
+        <>
+          <FontAwesomeIcon title="Neutral" icon={icons.sentiment.neutral} className="animate-fade-in-fast text-orange-500" />
+          <p>Neutral</p>
+        </>
       )}
     </span>
   );
